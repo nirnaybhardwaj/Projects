0774c03b6d8156cbf4adfa29068a0530ad3c1b0b
nirnaybhardwaj <nirnay99bhardwaj@gmail.com>
2022-05-24 18:16:16 +0530

    2nd commit

--- File System Organizer/FO.js
+++ File System Organizer/FO.js
@@ -1,9 +1,9 @@
-            //        DECRIPTION       //
+//        DECRIPTION       //
 
 // We will be creating a File System Organizer
 
 
-            // Features of the Project -  // 
+// Features of the Project -  // 
 
 // If you have numerous files in a folder and they are not Properly arranged
 //So you can use this tool to arrange them in specific directory or folder according to their extensions 
@@ -22,22 +22,22 @@ let input = process.argv.slice(2);
 
 let command = input[0]
 
-switch(command){
+switch (command) {
     case 'organize':
         organizeWaliFile.organizeFunKey(input[1]) //dirPath
         break;
-    case 'help' :
+    case 'help':
         console.log(`       List of all Commands - 
-                            1) Organize Command - node FO.js organize <dirname>
-                            2) Tree Command - node FO.js tree <dirPath>
-                            3) Help Command - node FO.js help`);
+1) Organize Command - node FO.js organize <dirname>
+2) Tree Command - node FO.js tree <dirPath>
+3) Help Command - node FO.js help`);
 
         break;
-    case 'tree' :
+    case 'tree':
 
-        break;    
-    default :
-        console.log('Enter a valid command')    
+        break;
+    default:
+        console.log('Enter a valid command')
 
 }
 
