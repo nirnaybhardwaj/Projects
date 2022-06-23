e6d8b57cec04c3659b4265f108665be9f8a3f073
nirnaybhardwaj <nirnay99bhardwaj@gmail.com>
2022-06-23 17:17:47 +0530

    2nd commit

--- Ticket management System/script.js
+++ Ticket management System/script.js
@@ -1,6 +1,8 @@
 
 let addBtn = document.querySelector('.add-button')
 let modalCont = document.querySelector('.model-cont')
+let mainCont = document.querySelector('.main-cont')
+let taskArea = document.querySelector('.textarea-cont')
 
 let addFlag = false
 
@@ -17,13 +19,39 @@ addBtn.addEventListener('click', function(){
     else{
         modalCont.style.display = 'none'
     }
-})
+});
 
+                // ---- GENTERATE THE TICKET ---- // 
 
+modalCont.addEventListener("keydown", function(e){
 
+    let key = e.key
 
+    if(key == 'Shift'){
+        createTicket(taskArea.value)
+        modalCont.style.display = 'none'
+        
+        addFlag = false // bcoz dubara whi s shuru ho agr nhi krenge yaha to ek ticket 
+        // bnne k baad jb + click krenge nhi aega model 2 baar click krne s aega 
 
+        taskArea.value = "" // pichla task likha na aye isliye empty string daaldi
+    }
+});
 
+function createTicket(ticketTask){
+    let ticketCont = document.createElement('div')
+    ticketCont.setAttribute('class', 'ticket-cont')
+
+    ticketCont.innerHTML = `<div class="ticket-color"></div>
+    <div class="ticket-id">id 1</div>
+    <div class="task-area">${ticketTask}</div>
+
+    <div class="ticket-lock">
+        <i class="fa-solid fa-lock"></i>
+    </div>`
+
+    mainCont.append(ticketCont)
+}
 
 
 
